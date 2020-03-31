cc6c37fb61790dae80249d3358b57926d8555852
yuancl0714 <yl541112@sina.com>
2020-03-31 13:54:22 +0800

    测试Redis

--- server/src/test/java/com/cl/learn/redis/test/RedisTest.java
+++ server/src/test/java/com/cl/learn/redis/test/RedisTest.java
@@ -12,6 +12,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.concurrent.TimeUnit;
+
 // 设置运行器
 @RunWith(SpringJUnit4ClassRunner.class)
 // 指明启动类
@@ -31,8 +33,19 @@ public class RedisTest {
     @Test
     public void method1() {
         log.info("------开始字符串的测试-------");
-        ValueOperations valueOperations = stringRedisTemplate.opsForValue();
-        valueOperations.set("SpringBoot:Redis:String:Key:V1", 10L);
+        final String key = "SpringBoot:Redis:String:Key:V1";
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, 10L);
+
+        // 获取key，value
+        log.info("--key={},value={}", key, valueOperations.get(key));
+
+        // redis加
+        Long aLong = valueOperations.increment(key, 5L);
+        log.info("---key={},value={},aLong={}", key, valueOperations.get(key), aLong);
+
+        // redis的key失效
+        redisTemplate.expire(key, 30L, TimeUnit.SECONDS);
     }
 
 }
