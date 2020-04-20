c909b078bdc15ff7a994165c497b2c7c6f0ef38e
yuancl0714 <yuancl_2012>
2020-04-20 22:40:34 +0800

    20190420

---
+++ interview/src/main/java/com/cl/interview/javabasic/jvm/model/ByteCodeSample.java
@@ -0,0 +1,13 @@
+package com.cl.interview.javabasic.jvm.model;
+
+/**
+ * @author chenLei
+ * @date 2020/4/18 15:48
+ */
+public class ByteCodeSample {
+    public static int add(int a, int b) {
+        int c = 0;
+        c = a + b;
+        return c;
+    }
+}
